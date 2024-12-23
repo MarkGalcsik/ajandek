4ba3b52caf03558d1233f3dea366718b30ac1e59
Galcsik Márk <mark.galcsik@stud.uni-corvinus.hu>
2024-12-23 19:03:19 +0100

    Add files via upload

---
+++ dorka.js
@@ -0,0 +1,5 @@
+document.addEventListener('click', () => {
+        document.getElementById('giftWrap').classList.add('open');
+        document.getElementById('giftWrap').classList.remove('joggle');
+    });
+
